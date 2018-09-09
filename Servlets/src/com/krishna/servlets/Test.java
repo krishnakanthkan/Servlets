33c19aeccc680c54ef0738e304c016ad63feb1fe
KrishnaKanth <krishnakanthkan@gmail.com>
2018-09-09 14:53:32 +0530

    initial

---
+++ Servlets/src/com/krishna/servlets/Test.java
@@ -0,0 +1,10 @@
+package com.krishna.servlets;
+
+public class Test {
+
+	public static void main(String[] args) {
+		//1 Initial
+
+	}
+
+}
---
+++ Servlets/src/com/krishna/servlets/WelcomeServlet.java
@@ -0,0 +1,34 @@
+package com.krishna.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	//first commmit
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<font size='6' color= green>");
+		out.println("<b>");
+		out.println("Hi, this is Krishna");
+		out.println("</b></font></body></html>");
+		
+	}
+
+}
